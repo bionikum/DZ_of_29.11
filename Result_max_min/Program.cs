e8dbf85cd28a02bbfdeaaf4d5396d4264b8be78d
Ilya <ilya@bobrikov.ru>
2022-11-30 09:13:40 +0500

    Max, min

---
+++ Result_max_min/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+{
+
+    Console.WriteLine($"Max: {x}");
+    Console.WriteLine($"Min: {y}" );
+}
+
+else
+{
+    Console.WriteLine($"Max: {x}");
+    Console.WriteLine($"Min: {y}");
+}
+
+
+
