e814131e189a800d7f6cc62f0056683319692a5a
Ilya <ilya@bobrikov.ru>
2022-11-30 12:47:35 +0500

    Max result of three humbers

---
+++ Rezult_max_of_three/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    if (c <= a)
+    {
+        Console.WriteLine($"Max: {a}");
+    }
+}
+if (b > c)
+{
+    if (a <= b)
+    {
+            Console.WriteLine($"Max: {b}");
+    }
+}
+if (b <= c)
+{
+    if (c > a)
+    {
+                Console.WriteLine($"Max: {c}");
+    }
+}
+if (a == b)
+{
+     if (a == c)
+     {
+        Console.WriteLine($"Max: {b + 0}");
+     }
+}
+
+
+
+
